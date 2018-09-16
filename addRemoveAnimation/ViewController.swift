346ead5911838bb887f93d3db5005f660d22372b
paulocardosob <paulocardosob@gmail.com>
2018-09-16 20:24:24 -0300

    Initial Commit

---
+++ addRemoveAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  addRemoveAnimation
+//
+//  Created by Paulo Jose on 16/09/18.
+//  Copyright © 2018 Paulo Jose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
